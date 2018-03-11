d270a4ce2a186d3e6aff4e4134ce3146c66ae48b
vugluskr86 <ivashkin.vasiliy@gmail.com>
2018-03-11 18:04:34 +0500

    Texts

--- Source/i18n.cpp
+++ Source/i18n.cpp
@@ -1,1 +1,13 @@
 #include "i18n.h"
+
+std::map<eBalancePropery, std::string> I18n::PROPERTIES = {
+   { IP_ATTACKPWMULTMAX, "Attack multiplier" },
+};
+
+std::map<eItemType, std::string> I18n::ITEM_TYPES = {};
+std::map<eSlotType, std::string> I18n::SLOT_TYPES = {};
+std::map<eDamageType, std::string> I18n::DAMAGE_TYPES = {};
+std::map<Direction, std::string> I18n::DIRECTIONS = {};
+std::map<eTileObjects, std::string> I18n::TILE_OBJECT_TYPES = {};
+std::map<eAbilityType, std::string> I18n::ABILITY_TYPES = {};
+std::map<eAbilityClassType, std::string> I18n::ABILITY_CLASS_TYPES = {};
--- Source/i18n.h
+++ Source/i18n.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "Types.h"
+
+class I18n {
+public:
+
+   static std::map<eBalancePropery, std::string> PROPERTIES;
+   static std::map<eItemType, std::string> ITEM_TYPES;
+   static std::map<eSlotType, std::string> SLOT_TYPES;
+   static std::map<eDamageType, std::string> DAMAGE_TYPES;
+   static std::map<Direction, std::string> DIRECTIONS;
+   static std::map<eTileObjects, std::string> TILE_OBJECT_TYPES;
+   static std::map<eAbilityType, std::string> ABILITY_TYPES;
+   static std::map<eAbilityClassType, std::string> ABILITY_CLASS_TYPES;
+};
